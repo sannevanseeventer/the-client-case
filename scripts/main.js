e10558efd67086edd2dba0ab6c9b027d8379df53
sannevanseeventer <sanne.van.seeventer@hva.nl>
2022-10-07 10:13:42 +0200

    new page

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,36 @@
-const button = document.getElementById("navLinks");
+var form1 = document.getElementById("form1");
+var form2 = document.getElementById("form2");
+var form3 = document.getElementById("form3");
 
-function showMenu(){
-    navLinks.styles.right = "0";
+var volgende1 = document.getElementById("volgende1");
+var volgende2 = document.getElementById("volgende2");
+var vorige1 = document.getElementById("vorige1");
+var vorige2 = document.getElementById("vorige2");
+
+var progress = document.getElementById("progress");
+
+volgende1.onclick = function (){
+    form1.style.left = "-400px";
+    form2.style.left = "5px";
+    progress.style.width = "240px";
 }
-function hideMenu(){
-    navLinks.styles.right = "-200px";
+
+vorige1.onclick = function (){
+    form1.style.left = "5px";
+    form2.style.left = "400px";
+    progress.style.width = "120px";
+}
+
+volgende2.onclick = function (){
+    form2.style.left = "-400px";
+    form3.style.left = "5px";
+    progress.style.width = "360px";
+}
+
+vorige2.onclick = function (){
+    form1.style.left = "-400px";
+    form2.style.left = "5px";
+    form3.style.left = "400px";
+    progress.style.width = "240px";
+
 }
